5e0bf02e2af2b94116839d3cdc59771691cf4f01
OkelloAbraham <39067559+OkelloAbraham@users.noreply.github.com>
2020-06-22 14:43:57 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-    res.send('hello world')
-  }
+// module.exports = (req, res) => {
+//     res.send('hello world')
+//   }
+
+var express = require('express')
+var app = express()
+
+app.get('/', (req, res)=>{
+    res.send('why always you')
+})
